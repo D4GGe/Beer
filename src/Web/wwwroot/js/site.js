56aa6b0b1f00005488f436c70861a14e1cfe8038
Daniel Fredriksson <D4GGe92@gmail.com>
2017-01-19 16:55:22 +0100

    Added search to the frontend

--- src/Web/wwwroot/js/site.js
+++ src/Web/wwwroot/js/site.js
@@ -7,6 +7,27 @@
 
     var ViewModel = function (data) {
         var self = this;
+        self.filter = ko.observable("");
+        data.forEach(function (pub) {
+            pub.filteredBeers = ko.computed(function () {
+                
+                var filter = self.filter();
+                if (!filter || filter == "None") {
+                    var ret = pub.beers;
+
+                    return ret;
+                } else {
+                    var ret = ko.utils.arrayFilter(pub.beers, function (i) {
+                        //console.log(i);
+                        return i.name.toLowerCase().search(filter.toLowerCase()) >= 0 || (i.type != null && i.type.toLowerCase().search(filter.toLowerCase()) >= 0) || (i.brewery != null && i.brewery.toLowerCase().search(filter.toLowerCase())) >= 0;
+                    });
+
+                    return ret;
+                }
+            });
+
+        })
+        
         self.pubs = ko.observableArray(data);
     }
 });
