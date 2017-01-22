2805130459464d5d4d93e7cb50e39ae5e57a73ea
Daniel Fredriksson <D4GGe92@gmail.com>
2017-01-22 16:06:01 +0100

    Added systemBolaget to frontend

--- src/Web/wwwroot/js/site.js
+++ src/Web/wwwroot/js/site.js
@@ -1,11 +1,14 @@
 ﻿$(document).ready(function () {
 
-    var jqxhr = $.getJSON( "/api/beers", function (data) {
-        ko.applyBindings(new ViewModel(data));
+    var jqxhr = $.getJSON("/api/beers", function (data) {
+        $.getJSON("/api/Releases", function (sysData) {
+            ko.applyBindings(new ViewModel(data, sysData));
+        })
+        
 
     })
 
-    var ViewModel = function (data) {
+    var ViewModel = function (data, sysData) {
         var self = this;
         self.filter = ko.observable("");
         data.forEach(function (pub) {
@@ -27,11 +30,45 @@
             });
 
         })
+        sysData.forEach(function (pub) {
+            pub.filteredBeers = ko.computed(function () {
+
+                var filter = self.filter();
+                if (!filter || filter == "None") {
+                    var ret = pub.beers;
+
+                    return ret;
+                } else {
+                    var ret = ko.utils.arrayFilter(pub.beers, function (i) {
+                        //console.log(i);
+                        return i.name.toLowerCase().search(filter.toLowerCase()) >= 0 || (i.type != null && i.type.toLowerCase().search(filter.toLowerCase()) >= 0) || (i.brewery != null && i.brewery.toLowerCase().search(filter.toLowerCase())) >= 0;
+                    });
+
+                    return ret;
+                }
+            });
+            var d = new Date(pub.date);
+            var datestring = d.getDate() + "/" + (d.getMonth() + 1) + " " + d.getFullYear();
+
+            pub.name = pub.type + " " + datestring;
+
+        })
         self.update = function () {
             $.get("api/beers/update", function (data) {
                 location.reload();
             });
         }
         self.pubs = ko.observableArray(data);
+        self.releases = ko.observableArray(sysData);
+        self.pubMode = ko.observable(true);
+        self.modeText = ko.observable("Systembolaget");
+        self.changeMode = function () {
+            self.pubMode(!self.pubMode());
+            if (self.pubMode()) {
+                self.modeText("Systembolaget");
+            } else {
+                self.modeText("Pubbar")
+            }
+        }
     }
 });
